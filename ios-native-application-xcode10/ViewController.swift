e752bac9f7d51402fbef927f33933d488659b0b7
Evgenii Khramkov <evkhramk@microsoft.com>
2018-06-07 16:19:29 +0300

    Initial Commit

---
+++ ios-native-application-xcode10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-native-application-xcode10
+//
+//  Created by Evgenii Khramkov on 6/7/18.
+//  Copyright © 2018 App Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
